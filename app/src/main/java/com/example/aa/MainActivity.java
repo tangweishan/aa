a8527e1aa4b99f3e29e7d1f2cb64bcee7f6b80be
tangweishan <61614174+tangweishan@users.noreply.github.com>
2020-04-21 12:03:33 +0800

    Update MainActivity.java
    22

--- app/src/main/java/com/example/aa/MainActivity.java
+++ app/src/main/java/com/example/aa/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String As ="dfj";
+         String q ="d";
     }
 }
