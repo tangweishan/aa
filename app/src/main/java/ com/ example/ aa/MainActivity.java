893619580c3bca9e88528859964793980342a245
tangweishan <924504178@qq.com>
2020-04-22 10:30:16 +0800

    欢迎

---
+++ app/src/main/java/ com/ example/ aa/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.aa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String As ="dfj";
+         String q ="d";
+        String w ="w";
+		String dx ="sdfj";
+    }
+}
